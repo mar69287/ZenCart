4c6598412b4444b81f019413b01758dbbad6e7db
mar69287 <marcoa.ruiz97@gmail.com>
2023-04-21 14:19:18 -0700

    create product card component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import { Card, CardBody, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import ReactStars from "react-rating-stars-component";
+
+const ProductCard = ({product}) => {
+  return (
+    <Card paddingTop={3} height={'21rem'}>
+        <Flex justify={'center'}>
+        <Image src={product.image}  boxSize="200px" />
+        </Flex>
+        <CardBody>
+        <Heading 
+        isTruncated
+        overflow="hidden"
+        textOverflow="ellipsis"
+        fontSize={'xl'}>{product.title}</Heading>
+        <ReactStars
+          count={5}
+          value={product.rating.rate}
+          size={20}
+          edit={false}
+        />
+            <Text fontSize={'md'} textAlign={'right'} paddingTop={2}>${product.price}</Text>
+        </CardBody>
+    </Card>
+  )
+}
+
+export default ProductCard
