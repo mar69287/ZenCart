68598eebb97cfa9751e4b9ad2c01e63bf8e95d9c
mar69287 <marcoa.ruiz97@gmail.com>
2023-04-21 08:33:32 -0700

    create auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,35 @@
 import SignUpForm from '../../components/SignUpForm/SignUpForm'
 import LoginForm from '../../components/LoginForm/LoginForm'
+import { useState } from 'react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 
 export default function AuthPage({ setUser }) {
+    const [showLogin, setShowLogin] = useState(true);
     return (
-        <main>
-            <h1>AuthPage</h1>
-            <SignUpForm setUser={setUser} />
-            <LoginForm setUser={setUser} />
-        </main>
+        <Box height="100vh" display="flex" alignItems="center">
+
+            <Flex
+                alignItems="center"
+                direction="column"
+            
+                justifyContent="center"
+                width="100%"
+            >
+                {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+                <Box mt={4}>
+                {showLogin ? (
+                    <Text>
+                    New to Fresh Finds?{' '}
+                    <Button size="sm" onClick={() => setShowLogin(false)}>Create your account</Button>
+                    </Text>
+                ) : (
+                    <Text>
+                    Already have an account?{' '}
+                    <Button size="sm" onClick={() => setShowLogin(true)}>Log in</Button>
+                    </Text>
+                )}
+                </Box>
+            </Flex>
+        </Box>
     )
 }
