e47849359832a8105feceacd4db503b085510fb7
mar69287 <marcoa.ruiz97@gmail.com>
2023-04-21 15:46:45 -0700

    create logo component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,14 +2,14 @@ import { HStack, Text, Box } from "@chakra-ui/react";
 import SearchInput from "../SearchInput";
 import {  BsFillCartFill } from 'react-icons/bs';
 import {  GiHamburgerMenu } from 'react-icons/gi';
-import { useState } from 'react';
 import SideDrawer from "../SideDrawer";
+import Logo from "../Logo";
 
 export default function NavBar({ isDrawerOpen, setIsDrawerOpen, handleUser, setSearchText }) {
 
     return (
         <HStack padding="10px">
-            <Text fontSize={'2xl'} fontFamily={'monospace'}>ZenCart</Text>
+            <Logo />
             <SearchInput setSearchText={setSearchText}/>
             <HStack spacing={4}>
                 <Box _hover={{ cursor: 'pointer', color: 'gray.500', transition: 'color 0.2s ease-in-out' }}>
