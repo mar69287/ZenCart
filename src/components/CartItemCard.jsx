08b8dce7197511e20040a2222bd2b6053b0ba14d
mar69287 <marcoa.ruiz97@gmail.com>
2023-04-22 21:11:07 -0700

    add purchase and continue shopping buttons to cart

--- src/components/CartItemCard.jsx
+++ src/components/CartItemCard.jsx
@@ -7,38 +7,39 @@ const CartItem = ({ item, cart, setCart }) => {
     setCart(updatedCart);
   };
 
-  const handleQuantityChange = (e) => {
-    const updatedCart = cart.map((cartItem) => {
-      if (cartItem.id === item.id) {
-        return { ...cartItem, quantity: e.target.value };
-      }
-      return cartItem;
-    });
-    setCart(updatedCart);
-  };
+  // const handleQuantityChange = (e) => {
+  //   const updatedCart = cart.map((cartItem) => {
+  //     if (cartItem.id === item.id) {
+  //       return { ...cartItem, quantity: e.target.value };
+  //     }
+  //     return cartItem;
+  //   });
+  //   setCart(updatedCart);
+  // };
 
   return (
-    <Box border="1px solid gray" borderRadius="md" padding="4" mt={'4'}>
-      <Flex justifyContent="space-between" alignItems="center" mb="2">
-        <Image src={item.image} boxSize={'70px'} mr="2" />
-        <Text 
-            fontWeight="bold"
-            isTruncated
-            overflow="hidden"
-            textOverflow="ellipsis"
-        >{item.title}</Text>
-        <VStack padding={'1'}>
-            <Text fontWeight="bold">${item.price}</Text>
-            <Button size="sm" onClick={handleRemove}>
-              Remove
-            </Button>
-        </VStack>
-      </Flex>
-      <Center>
-
-        <QuantityInput item={item} cart={cart} setCart={setCart} />  
-      </Center>
-    </Box>
+    <Center>
+      <Box border="1px solid gray" borderRadius="md" padding="4" mt={'4'} maxW={['100%', '90%', '70%']} minW={['100%', '90%', '70%']}>
+        <Flex justifyContent="space-between" alignItems="center" mb="2">
+          <Image src={item.image} boxSize={'70px'} mr="2" />
+          <Text
+              fontWeight="bold"
+              isTruncated
+              overflow="hidden"
+              textOverflow="ellipsis"
+          >{item.title}</Text>
+          <VStack padding={'1'}>
+              <Text fontWeight="bold">${item.price}</Text>
+              <Button size="sm" onClick={handleRemove}>
+                Remove
+              </Button>
+          </VStack>
+        </Flex>
+        <Center>
+          <QuantityInput item={item} cart={cart} setCart={setCart} />
+        </Center>
+      </Box>
+    </Center>
   );
 };
 
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,13 +1,34 @@
 import CartItemCard from "../components/CartItemCard";
 import MiniNav from "../components/MiniNav";
+import { HStack, VStack, Button, Text, Spacer } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const CartPage = ({ cart, setCart, isDrawerOpen, setIsDrawerOpen, handleUser }) => {
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
     return (
         <>
             <MiniNav isDrawerOpen={isDrawerOpen} setIsDrawerOpen={setIsDrawerOpen} handleUser={handleUser} />
             {cart.map(item => {
                 return <CartItemCard key={item.id} item={item} cart={cart} setCart={setCart} />
             })}
+            <VStack marginY={10} >
+                <HStack w="100%">
+                    <Spacer />
+                    <Text fontSize="lg" fontWeight="bold" mb={2}>Total: ${total.toFixed(2)}</Text>
+                </HStack>
+                <HStack w="100%" paddingBottom={'4'}>
+                    <Spacer />
+                    <Link to="/products">
+                        <Button colorScheme="gray" size="md">
+                        Continue Shopping
+                        </Button>
+                    </Link>
+                    <Button colorScheme="blue" size="md">
+                        Purchase
+                    </Button>
+                </HStack>
+            </VStack>
         </>
       )
 };
